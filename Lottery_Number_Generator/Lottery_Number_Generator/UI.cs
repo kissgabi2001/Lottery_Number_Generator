5964643c46dae5507a9f6307603bf36cf23f804f
kissgabi2001 <kissgabica2001@gmail.com>
2022-02-02 14:29:56 +0100

    Menu created

---
+++ Lottery_Number_Generator/Lottery_Number_Generator/UI.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery_Number_Generator
+{
+    class UI
+    {
+        public UI()
+        {
+            Menu();
+        }
+
+        private void Menu()
+        {
+            string text = "Lottery Number Generator";
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
+            Console.WriteLine("\nThis program generates random numbers to all the lottery types people plays in Hungary.\n" +
+                "From the options you can choose the game type you want to play.\n" +
+                "If you want to know more about the one of the games, you can read its description and rules after you have chosen.\n" +
+                "The type of the games:\n");
+            Console.WriteLine("\t[ 1. ] 'Ötöslottó'\n" +
+                "\n" +
+                "\t[ 2. ] 'Hatoslottó'\n" +
+                "\n" +
+                "\t[ 3. ] Scandinavian lottery\n" +
+                "\n" +
+                "\t[ 4. ] Eurojackpot\n" +
+                "\n" +
+                "\t[ 5. ] Luxor\n" +
+                "\n" +
+                "\t[ 6. ] 'Kenó'\n");
+            Console.WriteLine("Menu options:\n" +
+                "\n\t[ PRESS ANY KEY ] Close the program\n");
+            string t = "Please, select the type of lottery you want to play!";
+            for (int i = 0; i < t.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("\n" + t + "\nThe number of the game:");
+
+            int number = 0;
+
+            try
+            {
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Good luck! :)\n");
+                Environment.Exit(0);
+            }
+            switch (number)
+            {
+                case 1:
+                    //Otoslotto();
+                    break;
+                case 2:
+                    //Hatoslotto();
+                    break;
+                case 3:
+                    //Scandinavian();
+                    break;
+                case 4:
+                    //Eurojackpot();
+                    break;
+                case 5:
+                    //Luxor();
+                    break;
+                case 6:
+                    //Keno();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+    }
+}
